f007ff74eff4eabc739e5658080d9ceb7f229ead
Samuel Ishida <samuel.toyoshi@gmail.com>
2014-10-14 22:02:49 -0300

    fuck yea

--- tarefa4/quick.h
+++ tarefa4/quick.h
@@ -6,7 +6,12 @@ Turma A
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+
 #define MAX 50
 
+/*retorna o valor do custo*/
 double quick(int vet[], int tam);
+/*calcula o custo individual para o pivo escolhido*/
+double custoIndividual(int vet[], int tam, int pivo);
+/*separa a lista dos valores menores que o pivo e a lista dos valores maiores*/
+void sublistas(int vet[], int vetmenor[], int vetmaior[], int tam, int pivo, int *nMenor, int *nMaior);
