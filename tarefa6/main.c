b6f7e87266813804755fd899a1056d13acdca96d
Samuel Toyoshi Ishida <ra160250@cervantes.lab.ic.unicamp.br>
2014-10-29 17:42:16 -0200

    start

---
+++ tarefa6/main.c
@@ -0,0 +1,23 @@
+/*
+	Tarefa 6 de MC202
+	Aluno: Samuel Toyoshi Ishida
+	RA: 160250
+	Turma A
+*/
+
+
+
+int main()
+{
+	int nNos, numCasos;
+	char preOrdem[53], inOrdem[53];
+	
+	scanf("%d",&numCasos);
+	
+	while(numCasos--)
+	{
+		scanf("%d%s%s",&nNos,preOrdem,indOrdem);
+	}
+	
+	return 0;
+}
